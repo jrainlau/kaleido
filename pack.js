9e527e896a99f3316f1f9d30e2bcf5f16f3a9eaf
JrainLau <jrainlau@163.com>
2019-01-24 11:37:31 +0800

    feat: optimize packaging processing

---
+++ pack.js
@@ -0,0 +1,29 @@
+const { exec } = require('child_process')
+const { resolve } = require('path')
+const { argv } = require('yargs')
+
+const execCmd = (cmd) => {
+  return new Promise((resolve, reject) => {
+    console.log(`Running command "${cmd}"`)
+    exec(cmd, (err, stout, sterr) => {
+      if (err) reject(sterr)
+      console.log(stout)
+      resolve(stout)
+    })
+  })
+}
+
+const run = async () => {
+  const { platform } = argv
+  console.log(`Packaging platform: ${platform}`)
+  console.log(`Change directory to "${resolve(__dirname, './release')}"`)
+  process.chdir(resolve(__dirname, './release'))
+  await execCmd('npm install')
+  const buildCommand = platform === 'win'
+    ? 'electron-packager . Kaleido --platform=win32 --icon=./favicon.ico --overwrite'
+    : 'electron-packager . Kaleido --platform=darwin --icon=./icon.icns --overwrite'
+  await execCmd(buildCommand)
+  console.log('Packaging done!')
+}
+
+run()
--- package.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd release && npm i
-electron-packager . Kaleido --platform=darwin --icon=./icon.icns --overwrite
